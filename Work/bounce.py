7af2a37d1e98df3e155a85e961139e86c14eb731
RossBG86 <ross.oliver@bailliegifford.com>
2022-07-26 13:51:13 +0100

    updates 260722

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+dropDecrease = 3/5
+bounceMax = 10
+bounceCount = 1
+
+while bounceCount <= bounceMax:
+    print(bounceCount, round(height*dropDecrease,4))
+    height = height*dropDecrease
+    bounceCount = bounceCount + 1
+
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,28 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+extraPayment = 1000
+extraPaymentStart = 61
+extraPaymentEnd = 108
+total_paid = 0.0
+months = 0
+
+while principal > 0:
+    if months < extraPaymentEnd:
+        principal = principal * (1+rate/12) - payment - extraPayment
+        total_paid = total_paid + payment + extraPayment
+        months = months + 1
+        print(months, total_paid, principal)
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+        months = months + 1
+        print(months, total_paid, principal)
+
+print('Total paid: ', total_paid)
+print('Months: ', months)
+
+
